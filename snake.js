f89abae6229af508dddc6dcf3bfe041fbb504542
SeongOh Park <qkrtjddh1212@naver.com>
2022-04-29 11:23:23 +0900

    ADD do not move other side

--- snake.js
+++ snake.js
@@ -77,24 +77,29 @@ function clearScreen(){
 function keyDown(event){
     //keyboard left
     if(event.keyCode == 37){
+        if(xV == 1) return;
         xV = -1;
         yV = 0;
     }
     //keyboard up
     if(event.keyCode == 38){
+        if(yV == 1) return;
         xV = 0;
         yV = -1;
     }
     //keyboard right
     if(event.keyCode == 39){
+        if(xV == -1) return;
         xV = 1;
         yV = 0;
     }
     //keyboard down
     if(event.keyCode == 40){
+        if(yV == -1) return;
         xV = 0;
         yV = 1;
     }
+    //keyboard P
     if(event.keyCode == 80){
         if(isGaming){
             isGaming = false;
