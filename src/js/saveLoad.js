45622e59432a9c42c73cbcc9a61a39a0dd03db3b
Junghoon <kjh24871@naver.com>
2022-05-01 01:46:04 +0900

    saveData

--- src/js/saveLoad.js
+++ src/js/saveLoad.js
@@ -1,5 +1,5 @@
 let save_x = 0;
-document.getElementById("save_name").addEventListener('submit',saveSnake());
+document.getElementById("save_name").addEventListener("submit",saveSnake());
 
 
 
@@ -12,7 +12,7 @@ function saveSnake()
         i++;
     }
     console.log(i+(save_x%5));
-    localStorage.setItem("saveData"+((i+save_x)%5),JSON.stringify({name : document.querySelector("input").value, Score : score, snakeArray : snake, apple : apple}));  //새로운 div 창에서 입력된 값 저장
+    localStorage.setItem("saveData"+((i+save_x)%5),JSON.stringify({name : document.querySelector("#save_name input").value, Score : score, snakeArray : snake, apple : apple}));  //새로운 div 창에서 입력된 값 저장
     save_x++;
     /* to do : savePlayer가 무한대로 늘어날순없으니 최대 저장 갯수를 정하던가 선택한 칸에 저장하도록 하던가 */
 
